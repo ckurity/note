a9403d44423f883fca28f145644370e1ae4349af
ckurity <ckurity>
2023-07-25 12:13:52 +0800

    update

---
+++ Linux/Kali/dot zshrc_aliases.sh
@@ -0,0 +1,51 @@
+alias 1='ping google.com'
+alias 2='ping 8.8.8.8'
+alias 3='echo $ip'
+alias 4='ping $ip'
+alias 5='echo > ~/ip.txt $1'
+alias a=alias
+alias b=bash
+alias c=clear
+alias d=docker
+alias di='docker images'
+alias dp='docker ps'
+# alias docker=podman
+alias e=echo
+alias en='echo -n'
+alias eg=egrep
+alias egi='egrep -i'
+alias egn='egrep -n'
+alias f=file
+alias g=grep
+alias gi='grep -i'
+alias gn='grep -n'
+alias gv='grep -v'
+alias h=head
+alias i='ip -br -c a'
+alias l='ls -lh'
+alias la='ls -lha'
+alias lo=locate
+alias m=more
+alias nuclei='~/go/bin/nuclei'
+alias p=ping
+alias p8='ping 8.8.8.8'
+alias pg='ping google.com'
+alias po=poweroff
+alias pu='date; hostname -I | cut -d\  -f1; curl ifconfig.me'
+alias r=reboot
+alias s='ss -antu | nl -v0'
+alias smap='/home/kali/tools/smap_0.1.12_linux_amd64/smap'
+alias sp='ss -antup | nl -v0'
+alias t=tail
+alias u='sudo apt update -y && sudo apt upgrade -y'
+alias v='cat -n'
+alias waybackurls='~/go/bin/waybackurls'
+alias z=zsh
+
+# vi .zshrc
+
+# if [ -f ~/.zshrc_aliases ]; then
+#     . ~/.zshrc_aliases
+# fi
+
+# vi .zshrc_aliases
