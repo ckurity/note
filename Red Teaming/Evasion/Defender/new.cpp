7391d31ee61b14da6ac2439bb09125fb09dee340
ckurity <ckurity>
2023-11-29 01:36:23 -0500

    Update

--- Red Teaming/Evasion/Defender/new.cpp
+++ Red Teaming/Evasion/Defender/new.cpp
@@ -1,4 +1,23 @@
+#include <windows.h>
+
+#pragma comment (lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  ul_reason_for_call, LPVOID lpReserved) {
+
+    switch (ul_reason_for_call)  {
+    case DLL_PROCESS_ATTACH:
+    case DLL_PROCESS_DETACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}	
+
 extern "C" {
 __declspec(dllexport) BOOL WINAPI DllRegisterServer(void) {
 
 	MessageBox( NULL, "Welcome!", "Gemini Security", MB_OK );
+	return TRUE;
+	}
+}
